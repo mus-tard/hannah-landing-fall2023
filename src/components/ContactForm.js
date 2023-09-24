ef4f81a2eade37ae0d888003dacd6243a24dc17f
Charles K. Delahunt <120161030+mus-tard@users.noreply.github.com>
2023-09-24 01:31:01 -0400

    Contact 404 - more js by netilfy example

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { navigate } from gatsby
 import { 
     row,
     longMessage,
@@ -7,7 +8,35 @@ import {
     contactFormContainer,
  } from "./contactForm.module.css"
 
+ function encode(data) {
+    return Object.keys(data)
+      .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&')
+  }
+
 function ContactForm() {
+
+    const [state, setState] = React.useState({})
+
+    const handleChange = (e) => {
+        setState({ ...state, [e.target.name]: e.target.value })
+      }
+
+    const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+        }),
+    })
+        .then(() => navigate(form.getAttribute('action')))
+        .catch((error) => alert(error))
+    }
+
     return (
         <div className={contactFormContainer}>
             <div className={sectionIntro}>
@@ -19,9 +48,22 @@ function ContactForm() {
                     <li>Email: timothyryanhannah@gmail.com</li>
                 </ul>
             </div>
-            <form className={formContainer} action="/" name="contact" method="POST" data-netlify="true">
-            <input type="hidden" name="contact" value="contact" />
+            <form 
+                className={formContainer} 
+                name="contact"
+                action="/thanks/"
+                method="post" 
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={handleSubmit}
+            >
+            <input type="hidden" name="form-name" value="contact" />
             <div className={row}>
+            <p hidden>
+                <label>
+                    Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+                </label>
+            </p>
                 <p>
                     <label>Your Name: <input autocomplete="name" type="text" name="name" /></label>
                 </p>
---
+++ src/pages/thanks.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Logo from "../components/Logo" 
+import Footer from "../components/Footer" 
+
+const buttonStyles = {
+    backgroundColor: "#4057A3",
+    color: "#fff",
+    border: "none",
+    borderRadius: "10px",
+    width: "100%",
+    padding: "20px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    fontFamily: 'Open Sans',
+    fontStyle: "normal",
+    fontSize: "30px",
+    cursor: "pointer",
+    textDecoration: "none",
+}
+
+const pageStyles = {
+    color: "#fff",
+    padding: "96px",
+    fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  }
+
+export default () => (
+    <>
+        <main>
+            <Logo />
+            <h1 style={pageStyles}>Thank you!</h1>
+            <p>We will get back to you as soon as possible.</p>
+            <Link style={buttonStyles} to="/">Go back home</Link>
+        </main>
+        <Footer />
+    </>
+    
+
+)
