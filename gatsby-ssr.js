c172add1366c30794628f1fc1f73eb75c90aa3a7
Charles K. Delahunt <120161030+mus-tard@users.noreply.github.com>
2023-09-26 10:32:23 -0400

    Fix font warning message add crossOrigin

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,26 +6,31 @@ export const onRenderBody = ({ setHeadComponents }) => {
       rel="preload"
       href="/fonts/Microgramma-D-Extended-Bold.otf"
       as="font"
+      crossOrigin="anonymous"
     />,
     <link
       rel="preload"
       href="/fonts/AlegreyaSC-Italic.ttf"
       as="font"
+      crossOrigin="anonymous"
     />,
     <link
       rel="preload"
       href="/fonts/GildaDisplay-Regular.ttf"
       as="font"
+      crossOrigin="anonymous"
     />,
     <link
       rel="preload"
       href="/fonts/OpenSans-BoldItalic.ttf"
       as="font"
+      crossOrigin="anonymous"
     />,
     <link
       rel="preload"
       href="/fonts/OpenSans-Regular.ttf"
       as="font"
+      crossOrigin="anonymous"
     />,
   ])
 }
