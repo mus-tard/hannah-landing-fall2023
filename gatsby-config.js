9e4b311560b9f988a7e1691898c1c95d46e70b24
Charles K. Delahunt <120161030+mus-tard@users.noreply.github.com>
2023-09-26 10:15:08 -0400

    Add GAnalytics Tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
     options: {
       // You can add multiple tracking ids and a pageview event will be fired for all of them.
       trackingIds: [
-        "GA-TRACKING_ID", // Google Analytics / GA
+        "G-JZ0DK9V7L8", // Google Analytics / GA
         "AW-CONVERSION_ID", // Google Ads / Adwords / AW
         "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
       ],
